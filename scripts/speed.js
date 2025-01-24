9ed0fb4b5ab6dd24b040d0d1cc0178fe0a05a6cf
MohcineDev <mouhcinedev@gmail.com>
2025-01-24 12:32:07 +0100

    settttttttings

--- scripts/speed.js
+++ scripts/speed.js
@@ -1,8 +1,34 @@
-export const moveEnimiesX = 10
-export const moveEnimiesY = 10
+export let moveEnimiesX = 10
+export let moveEnimiesY = 10
 export const movePlayerSpeed = 5
 export const bulletSpeed = 10
 
+const HorInput = document.querySelector('.settings #hor input')
+const VerInput = document.querySelector('.settings #ver input')
+const minHor = document.querySelector('.settings #hor span:nth-of-type(1)')
+const maxHor = document.querySelector('.settings #hor span:nth-of-type(2)')
+const minVer = document.querySelector('.settings #ver span:nth-of-type(1)')
+const maxVer = document.querySelector('.settings #ver span:nth-of-type(2)')
+const horValue = document.querySelector('[for="hor"] em')
+const verValue = document.querySelector('[for="ver"] em')
+
+minHor.textContent = HorInput.min
+maxHor.textContent = HorInput.max
+
+minVer.textContent = VerInput.min
+maxVer.textContent = VerInput.max
+
+HorInput.oninput = (e) => {
+    console.log(e);
+    horValue.textContent = e.target.value
+    moveEnimiesX = parseInt(e.target.value)
+}
+VerInput.oninput = (e) => {
+    console.log(e);
+    verValue.textContent = e.target.value
+    moveEnimiesY = parseInt(e.target.value)
+
+}
 let index = 0
 export const writeTitle = (elem, txt) => {
     let a = txt.length
@@ -16,4 +42,5 @@ export const writeTitle = (elem, txt) => {
         }, 80)
 
     }
-} 
+}
+
